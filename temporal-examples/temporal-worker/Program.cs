3ecef815d8f3bfe864f407bc3c8314d5ba86bc53
James Strachan <jstrachan@scottlogic.com>
2025-07-24 10:59:06 +0100

    Adds a basic workflow and activity

---
+++ temporal-examples/temporal-worker/Program.cs
@@ -0,0 +1,34 @@
+﻿// Create a client to connect to localhost on "default" namespace
+using Temporalio.Client;
+using Temporalio.Worker;
+using workflows;
+
+var client = await TemporalClient.ConnectAsync(new("localhost:7233"));
+
+// Cancellation token to shutdown worker on ctrl+c
+using var tokenSource = new CancellationTokenSource();
+Console.CancelKeyPress += (_, eventArgs) =>
+{
+    tokenSource.Cancel();
+    eventArgs.Cancel = true;
+};
+
+var activities = new ExampleActivities();
+
+// Create a worker with the activity and workflow registered
+using var worker = new TemporalWorker(
+    client, // client
+    new TemporalWorkerOptions(taskQueue: "example")
+        .AddAllActivities(activities) // Register activities
+        .AddWorkflow<ExampleWorkflow>() // Register workflow
+);
+
+Console.WriteLine("Running worker...");
+try
+{
+    await worker.ExecuteAsync(tokenSource.Token);
+}
+catch (OperationCanceledException)
+{
+    Console.WriteLine("Worker cancelled");
+}
