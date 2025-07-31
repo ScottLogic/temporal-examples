2c1751a12b67d3c8874cbe43423367729268f915
Silas Benson <silasvb@gmail.com>
2025-07-31 14:18:46 +0100

    Create bespoke module

--- temporal-examples/temporal-worker/Program.cs
+++ temporal-examples/temporal-worker/Program.cs
@@ -2,18 +2,21 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Temporalio.Extensions.Hosting;
-using Temporalio.Client;
-using Temporalio.Runtime;
-using Temporalio.Worker;
 using workflows;
 using Microsoft.Extensions.DependencyInjection;
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
+using AutoFac.Modules;
 
 internal class Program
 {
     private static async Task Main(string[] args)
     {
         var host = Host.CreateDefaultBuilder(args)
-        .ConfigureLogging(ctx => ctx.AddSimpleConsole().SetMinimumLevel(LogLevel.Information))
+        .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+        .ConfigureContainer<ContainerBuilder>((context, builder) => {
+            builder.RegisterModule(new LoggingModule(LogLevel.Information));
+        })
         .ConfigureServices(ctx => 
             ctx.AddHostedTemporalWorker(
                 clientTargetHost: "host.docker.internal:7233",
