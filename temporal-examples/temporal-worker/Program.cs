3a431fc3a1d11c58c92381eff2e0442a49c50936
Silas Benson <silasvb@gmail.com>
2025-07-31 14:07:00 +0100

    Switch application to use a Host

--- temporal-examples/temporal-worker/Program.cs
+++ temporal-examples/temporal-worker/Program.cs
@@ -1,55 +1,31 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Temporalio.Extensions.Hosting;
 using Temporalio.Client;
 using Temporalio.Runtime;
 using Temporalio.Worker;
 using workflows;
+using Microsoft.Extensions.DependencyInjection;
 
-var config = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-    .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true)
-    .AddEnvironmentVariables() // This line enables environment variable overrides
-    .Build();
-
-var temporalUrl = config["Temporal:ClientUrl"] ?? "localhost:7233";
-
-var runtime = new TemporalRuntime(
-    new()
+internal class Program
+{
+    private static async Task Main(string[] args)
     {
-        Telemetry = new()
-        {
-            Metrics = new() { Prometheus = new PrometheusOptions("0.0.0.0:9000") },
-        },
+        var host = Host.CreateDefaultBuilder(args)
+        .ConfigureLogging(ctx => ctx.AddSimpleConsole().SetMinimumLevel(LogLevel.Information))
+        .ConfigureServices(ctx => 
+            ctx.AddHostedTemporalWorker(
+                clientTargetHost: "host.docker.internal:7233",
+                clientNamespace: "default",
+                taskQueue: "example")
+            .AddScopedActivities<ExampleActivities>()
+            .AddWorkflow<ExampleWorkflow>()
+            .AddWorkflow<ExampleWithChildrenWorkflow>()
+            .AddWorkflow<WaitingSignalWorkflow>()
+        )
+        .Build();
+
+        await host.RunAsync();
     }
-);
-
-var client = await TemporalClient.ConnectAsync(new(temporalUrl) { Runtime = runtime });
-
-// Cancellation token to shutdown worker on ctrl+c
-using var tokenSource = new CancellationTokenSource();
-Console.CancelKeyPress += (_, eventArgs) =>
-{
-    tokenSource.Cancel();
-    eventArgs.Cancel = true;
-};
-
-var activities = new ExampleActivities();
-
-// Create a worker with the activity and workflow registered
-using var worker = new TemporalWorker(
-    client,
-    new TemporalWorkerOptions(taskQueue: "example")
-        .AddAllActivities(activities)
-        .AddWorkflow<ExampleWorkflow>()
-        .AddWorkflow<ExampleWithChildrenWorkflow>()
-        .AddWorkflow<WaitingSignalWorkflow>()
-);
-
-Console.WriteLine("Running worker...");
-try
-{
-    await worker.ExecuteAsync(tokenSource.Token);
-}
-catch (OperationCanceledException)
-{
-    Console.WriteLine("Worker cancelled");
 }
