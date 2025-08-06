6f9bfa43b5e2a377110c1efec4e838d48a461224
Silas Benson <silasvb@gmail.com>
2025-08-06 15:11:38 +0100

    Resolve github copilot suggestions

--- temporal-examples/workflows/ExampleActivities.cs
+++ temporal-examples/workflows/ExampleActivities.cs
@@ -6,16 +6,16 @@ namespace Workflows;
 public class ExampleActivities
 {
     private readonly ILogger<ExampleActivities> _logger;
-    public ExampleActivities(ILogger<ExampleActivities> logg)
+    public ExampleActivities(ILogger<ExampleActivities> logger)
     {
-        _logger = logg;
+        _logger = logger;
     }
 
     [Activity]
     public async Task<string> GenericTask()
     {
         var duration = 2000;
-        _logger.LogInformation($"Starting delay: {@duration}");
+        _logger.LogInformation("Starting delay: {Duration}", duration);
         await Task.Delay(duration);
         _logger.LogInformation("Finished delay");
         return $"generic-task-{DateTime.Now}";
@@ -29,7 +29,7 @@ public class ExampleActivities
         for (int index = 0; index < numberOfChildWorkflows; index++)
         {
             var name = $"child-{index}-workflow-{DateTime.Now}";
-            _logger.LogInformation($"Creating child workflow {@name}");
+            _logger.LogInformation("Creating child workflow {Name}", name);
             childWorkflowsInfo.Add(name);
         }
         return childWorkflowsInfo;
